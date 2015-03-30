4d944a88eb7ff5a358c38b3aba79dfda168b6653
Naoki Endoh <naoki.endoh@gmail.com>
2015-03-30 15:56:52 +0900

    Add ListView

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,7 @@ var React = require('react-native');
 var {
   AppRegistry,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
@@ -21,25 +22,30 @@ var reactNativeSandbox = React.createClass({
       .then((response) => response.json())
       .then((responseData) =>{
         this.setState({
-          movies: responseData.movies
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true
         });
       })
       .done()
   },
   getInitialState: function() {
     return {
-      movies: null
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      }),
+      loaded: false
     }
   },
   componentDidMount: function() {
     this.fetchData();
   },
   render: function() {
-    if (!this.state.movies) {
+    if (!this.state.loaded) {
       return this.renderLoadingView();
     }
-    var movie = this.state.movies[0];
-    return this.renderMovie(movie);
+    return (
+      <ListView dataSource={this.state.dataSource} renderRow={this.renderMovie} style={styles.listView} />
+    );
   },
   renderLoadingView: function() {
     return (
@@ -64,6 +70,10 @@ var reactNativeSandbox = React.createClass({
 });
 
 var styles = StyleSheet.create({
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF'
+  },
   container: {
     flex: 1,
     flexDirection: 'row',
