554f43ee080421cbd8f6cd5f89b907bac03feffc
Naoki Endoh <naoki.endoh@gmail.com>
2015-04-23 19:18:59 +0900

    Add car detail temporary

--- CarDetail.js
+++ CarDetail.js
@@ -14,19 +14,37 @@ var API_URL = 'https://dev-api.anyca.net/api/cars/detail';
 var CarDetail = React.createClass({
   getInitialState() {
     return {
+      data: {},
       loaded: false
     }
   },
+  componentDidMount: function() {
+    this.fetchData();
+  },
   _urlForQueryAndPage() {
     return (
-      API_URL + '' + this.props.car.car_id
+      API_URL + '?car_id=' + this.props.car.car_id
     );
   },
+  fetchData: function() {
+    fetch(this._urlForQueryAndPage())
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData.car_data);
+        this.setState({
+          data: responseData.car_data,
+          loaded: true
+        });
+      })
+      .done()
+  },
   render() {
-    var car = this.props.car;
+    var car = this.state.data;
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>{car.car_name}</Text>
+      <View style={styles.container} id={car.car_id}>
+        <Text style={styles.text}>{car.model_name}</Text>
+        <Text style={styles.text}>{car.maker_name}</Text>
+        <Image source={{uri: car.image_url}} style={[{width: 320, height: 320}]} />
       </View>
     );
   }
@@ -42,7 +60,7 @@ var styles = StyleSheet.create({
     color: 'black',
     backgroundColor: 'pink',
     fontSize: 30,
-    margin: 80
+    margin: 20
   }
 });
 
